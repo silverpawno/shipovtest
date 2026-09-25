28a8610e2ea07e70c43138df7e5ff8a55533a6fc
silverpawno <disantisarab@gmail.com>
2026-09-26 01:00:10 +0500

    force update

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -27,7 +27,7 @@ export default function BackgroundMusic() {
       {/* Используем стабильную тестовую ссылку на короткий звуковой сигнал/трек */}
       <audio 
         ref={audioRef} 
-        src="https://github.com/silverpawno/shipovtest/releases/download/v1v1v1/bruh.mp3" 
+        src="https://github.com/silverpawno/shipovtest/releases/download/v1v1v1/Kanye_West_-_ALL_THE_LOVE_feat_Andre_Troutman_81159719.mp3" 
         loop 
         preload="auto" 
       />
